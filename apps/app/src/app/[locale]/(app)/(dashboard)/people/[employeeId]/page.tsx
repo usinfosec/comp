161bb1f944326809d0c0d6404affedccd8581bda
Mariano Fuentes <marfuen98@gmail.com>
2025-02-06 12:13:21 -0800

    async the params

--- apps/app/src/app/[locale]/(app)/(dashboard)/people/[employeeId]/page.tsx
+++ apps/app/src/app/[locale]/(app)/(dashboard)/people/[employeeId]/page.tsx
@@ -2,14 +2,11 @@ import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import { EmployeeDetails } from "./components/employee-details";
 
-interface PageProps {
-  params: {
-    employeeId: string;
-    locale: string;
-  };
-}
-
-export default async function EmployeeDetailsPage({ params }: PageProps) {
+export default async function EmployeeDetailsPage({
+  params,
+}: {
+  params: Promise<{ employeeId: string }>;
+}) {
   const session = await auth();
   const organizationId = session?.user.organizationId;
 
@@ -17,5 +14,7 @@ export default async function EmployeeDetailsPage({ params }: PageProps) {
     redirect("/");
   }
 
-  return <EmployeeDetails employeeId={params.employeeId} />;
+  const { employeeId } = await params;
+
+  return <EmployeeDetails employeeId={employeeId} />;
 }
