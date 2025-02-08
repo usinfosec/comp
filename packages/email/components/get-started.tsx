9c7adc5375b27f6ecce6e96929cd1bd47114eb23
Lewis Carhart <lewis@flexops.dev>
2025-02-08 12:00:59 +0000

    feat: introduction email on waitlist sign up w/ trigger.dev

--- packages/email/components/get-started.tsx
+++ packages/email/components/get-started.tsx
@@ -5,7 +5,7 @@ export function GetStarted() {
     <Section className="text-center mt-[50px] mb-[50px]">
       <Button
         className="bg-transparent text-primary text-[14px] text-[#121212] font-medium no-underline text-center px-6 py-3 border border-solid border-[#121212]"
-        href="https://trycomp.ai"
+        href="https://trycomp.ai?utm_source=email&utm_medium=get-started-button"
       >
         Get started
       </Button>
