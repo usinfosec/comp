9238b20eb86856292438757fe426ff6c6769fb0b
Lewis Carhart <lewis@flexops.dev>
2025-02-19 22:20:08 +0000

    fix: style of unauthorized page

--- apps/portal/src/app/[locale]/(app)/unauthorized/page.tsx
+++ apps/portal/src/app/[locale]/(app)/unauthorized/page.tsx
@@ -7,11 +7,11 @@ export default async function Unauthorized() {
   });
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div>
+      <h1>
         We couldn't find an organization for you. Please contact your
         administrator.
-      </main>
+      </h1>
     </div>
   );
 }
