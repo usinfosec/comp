2f29525495aae1cff1e75fee27feb4a0552eb243
Lewis Carhart <lewis@flexops.dev>
2025-02-19 22:20:24 +0000

    fix: style of unauthorized page

--- apps/portal/src/app/[locale]/(app)/unauthorized/page.tsx
+++ apps/portal/src/app/[locale]/(app)/unauthorized/page.tsx
@@ -1,11 +1,4 @@
-import { auth } from "@/app/lib/auth";
-import { headers } from "next/headers";
-
 export default async function Unauthorized() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-
   return (
     <div>
       <h1>
