8a3d066658824ae96f018f48679976c428b75c14
Mariano Fuentes <marfuen98@gmail.com>
2025-02-24 12:58:41 -0800

    fix posthog hydration error

--- packages/analytics/src/components/page-view.tsx
+++ packages/analytics/src/components/page-view.tsx
@@ -2,30 +2,27 @@
 
 import { usePathname, useSearchParams } from "next/navigation";
 import { usePostHog } from "posthog-js/react";
-import { Suspense, useEffect } from "react";
+import { useEffect } from "react";
 
-function PageViewTracker(): null {
+export function PostHogPageView(): null {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const posthog = usePostHog();
 
+  // Track pageviews
   useEffect(() => {
     if (pathname && posthog) {
-      let url = window.origin + pathname;
-      if (searchParams.toString()) {
-        url = `${url}?${searchParams.toString()}`;
-      }
-      posthog.capture("$pageview", { $current_url: url });
+      // Track page views
+      const url = searchParams.toString()
+        ? `${pathname}?${searchParams.toString()}`
+        : pathname;
+
+      // Track pageview with current URL
+      posthog.capture("$pageview", {
+        $current_url: url,
+      });
     }
   }, [pathname, searchParams, posthog]);
 
   return null;
-}
-
-export function PostHogPageView() {
-  return (
-    <Suspense>
-      <PageViewTracker />
-    </Suspense>
-  );
 }
