8dde2c4b82f5ee690ccb6851ae8c921392faba88
7yntax <kris@trycomp.ai>
2025-03-11 21:21:54 +0000

    COM-20 fixed typo

--- apps/app/src/app/[locale]/(app)/(dashboard)/tests/all/[testId]/actions/getTest.ts
+++ apps/app/src/app/[locale]/(app)/(dashboard)/tests/all/[testId]/actions/getTest.ts
@@ -7,8 +7,7 @@ import {
   type ActionResponse 
 } from "./types";
 
-import type type { Test } from "../../types";
-import { OrganizationIntegration } from "@/components/integrations/integrations";
+import type { Test } from "../../types";
 
 export async function getTest(input: { testId: string }): Promise<ActionResponse<Test>> {
   const { testId } = input;
