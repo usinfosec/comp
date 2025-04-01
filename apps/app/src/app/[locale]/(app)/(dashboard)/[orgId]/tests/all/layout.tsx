8505c559d4b8bf1da66044bd962169b1cb43fbd3
7yntax <kris@trycomp.ai>
2025-04-01 21:51:46 +0100

    COM-20 added added onboarding component

--- apps/app/src/app/[locale]/(app)/(dashboard)/[orgId]/tests/all/layout.tsx
+++ apps/app/src/app/[locale]/(app)/(dashboard)/[orgId]/tests/all/layout.tsx
@@ -1,7 +1,9 @@
 import { auth } from "@/auth";
 import { getI18n } from "@/locales/server";
 import { SecondaryMenu } from "@bubba/ui/secondary-menu";
-
+import { AppOnboarding } from "@/components/app-onboarding";
+import { db } from "@bubba/db";
+import { cache, Suspense } from "react";
 export default async function Layout({
 	children,
 }: {
@@ -10,6 +12,40 @@ export default async function Layout({
 	const t = await getI18n();
 	const session = await auth();
 	const organizationId = session?.user?.organizationId;
+
+	const tests = await getTestsOverview();
+	console.log(tests);
+
+	if (!tests.length) {
+		return (
+			<div className="max-w-[1200px] m-auto">
+				<Suspense fallback={<div>Loading...</div>}>
+					<div className="mt-8">
+						<AppOnboarding
+							title={t("app_onboarding.cloud_tests.title")}
+							description={t("app_onboarding.cloud_tests.description")}
+							imageSrc="/onboarding/cloud-tests.png"
+							imageAlt="Cloud Security Testing"
+							faqs={[
+								{
+									questionKey: t("app_onboarding.cloud_tests.faqs.question_1"),
+									answerKey: t("app_onboarding.cloud_tests.faqs.answer_1"),
+								},
+								{
+									questionKey: t("app_onboarding.cloud_tests.faqs.question_2"),
+									answerKey: t("app_onboarding.cloud_tests.faqs.answer_2"),
+								},
+								{
+									questionKey: t("app_onboarding.cloud_tests.faqs.question_3"),
+									answerKey: t("app_onboarding.cloud_tests.faqs.answer_3"),
+								},
+							]}
+						/>
+					</div>
+				</Suspense>
+			</div>
+		);
+	}
 
 	return (
 		<div className="max-w-[1200px] mx-auto">
@@ -29,3 +65,16 @@ export default async function Layout({
 		</div>
 	);
 }
+
+const getTestsOverview = cache(async () => {
+	const session = await auth();
+	const orgId = session?.user.organizationId;
+
+	const tests = await db.organizationIntegrationResults.findMany({
+		where: {
+			organizationId: orgId,
+		},
+	});
+
+	return tests;
+});
