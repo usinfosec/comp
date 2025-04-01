9aeefe551b57f5316548ae87a190d501b3f36cf6
Lewis Carhart <lewis@flexops.dev>
2025-04-01 18:01:53 -0400

    refactor: remove unused UpdateOrganizationWebsite component and enhance session handling in layout

--- apps/app/src/app/[locale]/(public)/layout.tsx
+++ apps/app/src/app/[locale]/(public)/layout.tsx
@@ -1,7 +1,18 @@
+import { auth } from "@bubba/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session?.session.activeOrganizationId) {
+    redirect(`/${session.session.activeOrganizationId}/overview`)
+  }
   return <>{children}</>;
 }
