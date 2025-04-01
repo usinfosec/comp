a62d0fbed3be2f872d1e77cf30bbef6e2cb7b43f
Claudio Fuentes <imclaudfuen@gmail.com>
2025-04-01 17:46:42 -0400

    refactor: enhance risk retrieval and deletion logic to include task fetching and improve error handling

--- packages/db/src/index.js
+++ packages/db/src/index.js
@@ -4,16 +4,15 @@ exports.db = void 0;
 const client_1 = require("@prisma/client");
 // import { generateIdExtension } from "./extensions/generate-id-extension";
 const createPrismaClient = () => {
-    return new client_1.PrismaClient({
-        datasources: {
-            db: {
-                url: process.env.DATABASE_URL,
-            },
-        },
-        log: ["error", "warn"],
-    });
+	return new client_1.PrismaClient({
+		datasources: {
+			db: {
+				url: process.env.DATABASE_URL,
+			},
+		},
+		log: ["error", "warn"],
+	});
 };
 const globalForPrisma = globalThis;
 exports.db = globalForPrisma.prisma ?? createPrismaClient();
-if (process.env.NODE_ENV !== "production")
-    globalForPrisma.prisma = exports.db;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = exports.db;
