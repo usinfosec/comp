f7108942006518682d13e7a8fa621b7872a4066a
Mariano Fuentes <marfuen98@gmail.com>
2025-04-14 21:29:04 -0400

    chore: update Prisma dependencies and configuration
    - Upgraded Prisma packages to version 6.6.0 in bun.lock, yarn.lock, and package.json for improved functionality and performance.
    - Adjusted Prisma schema path in package.json for better organization.
    - Introduced a new Prisma schema file for enhanced database management.
    - Updated import paths in index.js and index.ts to reference the generated Prisma client.

--- packages/db/src/index.js
+++ packages/db/src/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
-const client_1 = require("@prisma/client");
+const client_1 = require("../generated/prisma/client");
 const createPrismaClient = () => {
     return new client_1.PrismaClient({
         datasources: {
