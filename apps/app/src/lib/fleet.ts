cfd8dfc394a7e5a705a8c13a8d7f65f716cf115a
Mariano Fuentes <marfuen98@gmail.com>
2025-06-11 11:58:39 -0400

    chore: define env var properly

--- apps/app/src/lib/fleet.ts
+++ apps/app/src/lib/fleet.ts
@@ -1,10 +1,7 @@
+import { env } from "@/env.mjs";
 import axios from "axios";
 
-if (!process.env.FLEET_URL || !process.env.FLEET_TOKEN) {
-  throw new Error("FLEET_URL or FLEET_TOKEN not found");
-}
-
 export const fleet = axios.create({
-  baseURL: `${process.env.FLEET_URL}/api/v1/fleet`,
-  headers: { Authorization: `Bearer ${process.env.FLEET_TOKEN}` },
+  baseURL: `${env.FLEET_URL}/api/v1/fleet`,
+  headers: { Authorization: `Bearer ${env.FLEET_TOKEN}` },
 });
