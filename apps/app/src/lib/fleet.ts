a955e6a113592726eadf6ab40573b4d6de21e215
Mariano Fuentes <marfuen98@gmail.com>
2025-06-15 18:45:05 -0700

    chore: update fleet url

--- apps/app/src/lib/fleet.ts
+++ apps/app/src/lib/fleet.ts
@@ -1,7 +1,6 @@
-import { env } from "@/env.mjs";
 import axios from "axios";
 
 export const fleet = axios.create({
-  baseURL: `${env.FLEET_URL}/api/v1/fleet`,
-  headers: { Authorization: `Bearer ${env.FLEET_TOKEN}` },
+  baseURL: `${process.env.FLEET_URL}/api/v1/fleet`,
+  headers: { Authorization: `Bearer ${process.env.FLEET_TOKEN}` },
 });
